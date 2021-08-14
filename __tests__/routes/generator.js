7a60ddf799cdf301b708541049a594220859d746
Diego Da'Silva <smartcrashomg@gmail.com>
2021-08-14 10:35:49 -0400

    Update tests

--- __tests__/routes/generator.js
+++ __tests__/routes/generator.js
@@ -4,6 +4,8 @@ const { resolve } = require('path')
 
 const createRequest = () => request(app).post('/generator')
 
+const getFile = fileName => resolve(`__tests__/routes/files/${fileName}`)
+
 const assertErrorResponse = (res, expectedMessage) => {
   expect(res.body).toEqual({
     error: true,
@@ -14,16 +16,16 @@ const assertErrorResponse = (res, expectedMessage) => {
 describe('POST /generator', () => {
   describe('only accepts PNG and JPEG files', () => {
     it('allows PNG files', done => {
-      createRequest().attach('image', resolve('__tests__/routes/files/a-png.png')).expect(200).end(done)
+      createRequest().attach('image', getFile('a-png.png')).expect(200).end(done)
     })
 
     it('allows JPG/JPEG files', done => {
-      createRequest().attach('image', resolve('__tests__/routes/files/a-jpg.jpg')).expect(200).end(done)
+      createRequest().attach('image', getFile('a-jpg.jpg')).expect(200).end(done)
     })
 
     it('does not allow any other file type', done => {
       createRequest()
-        .attach('image', resolve('__tests__/routes/files/a-pdf.pdf'))
+        .attach('image', getFile('a-pdf.pdf'))
         .expect(400)
         .expect('Content-Type', /json/)
         .expect(res => assertErrorResponse(res, `Invalid file extension. Allowed extensions: PNG, JPG, JPEG`))
@@ -33,7 +35,7 @@ describe('POST /generator', () => {
 
   it('must reject input file bigger than 5mb', done => {
     createRequest()
-      .attach('image', resolve('__tests__/routes/files/a-very-large-file.png'))
+      .attach('image', getFile('a-very-large-file.png'))
       .expect(400)
       .expect('Content-Type', /json/)
       .expect(res => assertErrorResponse(res, 'The image is too large. Max size 5MB'))
