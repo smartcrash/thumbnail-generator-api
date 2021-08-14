3071c0dfc57bd342af2c8c8e70f1488223fd69d7
Diego Da'Silva <smartcrashomg@gmail.com>
2021-08-14 11:44:14 -0400

    Implement image resizing

--- __tests__/routes/generator.js
+++ __tests__/routes/generator.js
@@ -6,6 +6,27 @@ const createRequest = () => request(app).post('/generator')
 
 const getFile = fileName => resolve(`__tests__/routes/files/${fileName}`)
 
+const assertSuccessResponse = res => {
+  const { success, id, thumbnails } = res.body
+
+  console.log(`res.body`, res.body)
+
+  expect(success).toBe(true)
+  expect(typeof id).toBe('string')
+  expect(thumbnails).toHaveLength(3)
+
+  for (const thumbnail of thumbnails) {
+    expect(typeof thumbnail).toBe('string')
+    expect(thumbnail).toContain(id)
+    expect(thumbnail).toMatch(/(http:|https:)+[^\s]+[\w]/)
+  }
+
+  // Each element of "thumbnails" is an URL pointing to that image
+  expect(thumbnails[0]).toContain('400x300')
+  expect(thumbnails[1]).toContain('160x120')
+  expect(thumbnails[2]).toContain('120x120')
+}
+
 const assertErrorResponse = (res, expectedMessage) => {
   expect(res.body).toEqual({
     error: true,
@@ -39,6 +60,15 @@ describe('POST /generator', () => {
       .expect(400)
       .expect('Content-Type', /json/)
       .expect(res => assertErrorResponse(res, 'The image is too large. Max size 5MB'))
+      .end(done)
+  })
+
+  it('gives 3 new images with the following dimensions: 400x300, 160x120, 120x120', done => {
+    createRequest()
+      .attach('image', getFile('a-png.png'))
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(assertSuccessResponse)
       .end(done)
   })
 })
--- routes/generator.js
+++ routes/generator.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const storage = multer.memoryStorage()
+const upload = multer({ dest: 'uploads/', storage })
+const { v4: uuidv4 } = require('uuid')
+const { writeFile, mkdir } = require('fs').promises
+const { resolve } = require('path')
+const sharp = require('sharp')
+
+const fullURL = req => req.protocol + '://' + req.get('host')
 
 const allowMimeTypes = ['image/jpeg', 'image/jpg', 'image/png']
 const validateMimeType = (mimetype = '') => allowMimeTypes.includes(mimetype)
+
+const createThumbnails = (buffer, sizes) => {
+  const buffers = Promise.all(
+    sizes.map(size => {
+      return sharp(buffer)
+        .resize(...size)
+        .jpeg({ mozjpeg: true })
+        .toBuffer()
+    })
+  )
+
+  return buffers
+}
 
 /**
  * @openapi
@@ -15,9 +35,9 @@ const validateMimeType = (mimetype = '') => allowMimeTypes.includes(mimetype)
  *       200:
  *         description: Returns converted image in varioures dimentions
  */
-router.post('/', upload.single('image'), function (req, res, next) {
+router.post('/', upload.single('image'), async (req, res, next) => {
   const { file } = req
-  const { mimetype, size: sizeInBytes } = file
+  const { mimetype, size: sizeInBytes, buffer: fileBuffer } = file
   const sizeInMB = sizeInBytes / (1024 * 1024)
 
   if (!validateMimeType(mimetype)) {
@@ -36,11 +56,38 @@ router.post('/', upload.single('image'), function (req, res, next) {
     return
   }
 
-  res.json({
-    status: 200,
-    error: '',
-    message: 'No message available',
-  })
+  try {
+    const dimentions = [
+      [400, 300],
+      [160, 120],
+      [120, 120],
+    ]
+
+    const id = uuidv4()
+    const url = fullURL(req)
+    const buffers = await createThumbnails(fileBuffer, dimentions)
+
+    const directory = resolve(`public/thumbnails/${id}`)
+    await mkdir(directory, { recursive: true })
+    await Promise.all([
+      writeFile(`${directory}/400x300.jpeg`, buffers[0]),
+      writeFile(`${directory}/160x120.jpeg`, buffers[1]),
+      writeFile(`${directory}/120x120.jpeg`, buffers[2]),
+    ])
+
+    const thumbnails = [`${url}/${id}/400x300`, `${url}/${id}/160x120`, `${url}/${id}/120x120`]
+
+    res.status(200).json({
+      success: true,
+      id,
+      thumbnails,
+    })
+  } catch (error) {
+    res.status(400).json({
+      error: true,
+      message: 'Oops! Something went wrong, please try later.',
+    })
+  }
 })
 
 module.exports = router
