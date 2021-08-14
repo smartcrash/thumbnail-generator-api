6d518e6d281601fcd1191745111395992cd1ed66
Diego Da'Silva <smartcrashomg@gmail.com>
2021-08-14 08:24:58 -0400

    Reduce schafold code

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -18,15 +15,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+/** GET home page. */
+app.get('/', (_, res) =>  res.render('index', {}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+/** Catch 404 and forward to error handler */ 
+app.use((_, __, next) =>  next(createError(404)));
 
-// error handler
-app.use(function(err, req, res, next) {
+/** Error handler */
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
